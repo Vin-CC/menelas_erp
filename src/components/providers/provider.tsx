8198a4f376a177a22870445a95980ab341a4e2de
Charon <omaralmoctar.coulibaly@edu.ece.fr>
2024-07-25 10:01:42 +0200

    The @tanstack/react-query has been removed and also the home page has been removed from the public route

--- src/components/providers/provider.tsx
+++ src/components/providers/provider.tsx
@@ -2,16 +2,11 @@
 
 import { SessionProvider } from "next-auth/react";
 import React from 'react'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
-const queryClient = new QueryClient()
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
         <SessionProvider>
-            <QueryClientProvider client={queryClient}>
-                {children}
-            </QueryClientProvider>
+            {children}
         </SessionProvider>
     )
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,4 @@
 export const publicRoutes = [
-    "/",
 ];
 
 export const authRoutes = [
