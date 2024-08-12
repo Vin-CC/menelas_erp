0f80daf543aa6572deb29fe820f646b09275b639
Vince <vincent.cance@gmail.com>
2024-08-12 13:52:14 +0200

    Revert "page projets - créer page #5"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import Providers from "@/components/providers/provider";
-import { Toaster } from "@/components/ui/toaster"
-
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,19 +16,15 @@ export default function RootLayout({ children, }: Readonly<{ children: React.Rea
     <html lang="fr">
       <body className={inter.className}>
         <Providers>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <main>
-              {children}
-              <Toaster />
-            </main>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <div className="flex h-screen">
+              <main className="flex-1 overflow-auto bg-white dark:bg-gray-900">
+                {children}
+              </main>
+            </div>
           </ThemeProvider>
         </Providers>
-      </body >
-    </html >
+      </body>
+    </html>
   );
 }
